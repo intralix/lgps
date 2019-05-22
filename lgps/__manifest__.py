cdf456b036774c3be89d38d9a06672c24efc2ff6
javina <sistemas@intralix.com>
2019-05-22 15:27:25 -0500

    Force update

--- lgps/__manifest__.py
+++ lgps/__manifest__.py
@@ -6,7 +6,7 @@
     'application': True,
     'website': 'https://www.intralix.com',
     'category': 'Uncategorized',
-    'version': '0.0.8',
+    'version': '0.0.9',
     'depends': [
         'base',
         'stock',
