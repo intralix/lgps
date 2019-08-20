6345664578f6af3d5ffec79bfa9756c16e7b03f3
javina <sistemas@intralix.com>
2019-08-20 11:50:16 -0500

    change version

--- lgps/__manifest__.py
+++ lgps/__manifest__.py
@@ -6,7 +6,7 @@
     'application': True,
     'website': 'https://www.intralix.com',
     'category': 'Uncategorized',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'depends': [
         'base',
         'stock',
