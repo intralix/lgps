c4387b08e9538b9f824f28d940a2b2fa50bebd7a
javina <sistemas@intralix.com>
2020-01-07 17:23:09 -0600

    Changes in version file

--- lgps/__manifest__.py
+++ lgps/__manifest__.py
@@ -6,7 +6,7 @@
     'application': True,
     'website': 'https://www.intralix.com',
     'category': 'Uncategorized',
-    'version': '0.2.1',
+    'version': '0.2.2',
     'depends': [
         'base',
         'stock',
