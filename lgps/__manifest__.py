f9b95e0a035b51b0286a72ce3ddce88cc547071f
javina <sistemas@intralix.com>
2020-08-21 17:06:07 -0500

    Added security rules

--- lgps/__manifest__.py
+++ lgps/__manifest__.py
@@ -6,7 +6,7 @@
     'application': True,
     'website': 'https://www.intralix.com',
     'category': 'Uncategorized',
-    'version': '0.4.0',
+    'version': '0.4.1',
     'depends': [
         'base',
         'stock',
