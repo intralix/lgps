827a2c10a2525ffcb839f010b659a88c7e8a6c39
javina <sistemas@intralix.com>
2021-01-28 19:44:45 -0600

    Fix write method and implement create method

--- lgps_margins/models/so_margin.py
+++ lgps_margins/models/so_margin.py
@@ -17,6 +17,12 @@ class CheckMarginOnSalesOrder(models.Model):
         store=True,
     )
 
+    sacrifice_sale = fields.Boolean(
+        string=_("Sell at a loss"),
+        default=False,
+        help=_('Allow to Sell at a loss')
+    )
+
     @api.one
     @api.depends('margin', 'amount_untaxed')
     def _compute_margin_percent(self):
@@ -25,43 +31,37 @@ class CheckMarginOnSalesOrder(models.Model):
         else:
             self.margin_percent = (self.margin / self.amount_untaxed)
 
+    @api.model
+    def create(self, values):
+        _logger.warning('Calling Created Method')
+        _logger.warning('%s', self)
+        new_record = super(CheckMarginOnSalesOrder, self).create(values)
+        new_record.check_rules_on_sales_order()
+        return new_record
+
     @api.multi
-    def _write(self, values):
-        # _logger.error('Calling Write Method')
+    def write(self, values):
+        _logger.warning('Calling Write Method')
+        _logger.warning('%s', self)
+
         for po in self:
-            po._check_rules_on_sales_order()
-        # _logger.error('Already Saved. Checking now')
-        return super(CheckMarginOnSalesOrder, self)._write(values)
+            _logger.info('Running for PO %s', po.id)
+            po.check_rules_on_sales_order()
 
-    def _check_rules_on_sales_order(self):
+        return super(CheckMarginOnSalesOrder, self).write(values)
 
+    def check_rules_on_sales_order(self):
         # Gettin Max discount Rule
         user = self.env.user
-        _logger.error('Trabajando con el usuario : %s', self.env.user)
-        if user.id == 1:
-            if self.user_id:
-                user = self.env['res.users'].search([['id', '=', self.user_id.id]], limit=1)
-                _logger.error('Cambiando a el usuario : %s', user)
-
+        _logger.info('Trabajando con el usuario : %s', self.env.user)
         max_discount_allowed = round(user.min_margin * 100)
-        skip_min_margin_rule = user.skip_min_margin_rule
-        lower_margin = 1.10
+        _logger.info('Max discount allowed : %s', max_discount_allowed)
 
-        _logger.error('self : %s', self)
-        # _logger.error('self.env.uid : %s', self.env.uid)
-        # _logger.error('self.env.user.name : %s', user.name)
-        # _logger.error('self.env.user.email : %s', user.email)
-        # _logger.error('self.env.user.company_id.id : %s', user.company_id.id)
-        _logger.error('max_discount_allowed : %s', max_discount_allowed)
-        _logger.error('skip_min_margin_rule : %s', skip_min_margin_rule)
-        #_logger.error('vendedor : %s', self.user_id)
-        # raise UserError('Excepcón controlada para ver variables')
-        # _logger.warning('===============================================')
         order = self
         amount_untaxed = amount_tax = 0.0
         internal_margin = 0
 
-        _logger.error('Current order %s', order)
+        _logger.info('Current order %s', order)
         for line in order.order_line:
 
             amount_untaxed += line.price_subtotal
@@ -70,16 +70,16 @@ class CheckMarginOnSalesOrder(models.Model):
             internal_margin += line_margin
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
 
-            # _logger.warning('Line :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
-            # _logger.warning('product: %s', line.product_id.name)
-            # _logger.warning('quantity: %s', line.product_uom_qty)
-            # _logger.warning('price_unit: %s', line.price_unit)
-            # _logger.warning('purchase_price: %s', line.purchase_price)
-            # _logger.warning('price_tax: %s', line.price_tax)
-            # _logger.warning('discount: %s', line.discount)
-            # _logger.warning('price_subtotal: %s', line.price_subtotal)
-            # _logger.warning('margin: %s', line_margin)
-            # _logger.warning('price_total: %s', line.price_total)
+            _logger.info('Line :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
+            _logger.info('product: %s', line.product_id.name)
+            _logger.info('quantity: %s', line.product_uom_qty)
+            _logger.info('price_unit: %s', line.price_unit)
+            _logger.info('purchase_price: %s', line.purchase_price)
+            _logger.info('price_tax: %s', line.price_tax)
+            _logger.info('discount: %s', line.discount)
+            _logger.info('price_subtotal: %s', line.price_subtotal)
+            _logger.info('margin: %s', line_margin)
+            _logger.info('price_total: %s', line.price_total)
 
         amount_total = amount_untaxed + amount_tax
         if amount_untaxed > 0:
@@ -87,27 +87,22 @@ class CheckMarginOnSalesOrder(models.Model):
         else:
             margin_percent = 0
 
-        _logger.error('Base Imponible : %s', amount_untaxed)
-        _logger.error('Impuestos : %s', amount_tax)
-        _logger.error('Total : %s', amount_total)
-        _logger.error('Margin : %s', internal_margin)
-        _logger.error('Margin in Percent: %s', margin_percent)
-        _logger.error('amount_untaxed: %s', order.amount_untaxed)
+        _logger.info(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
+        _logger.info('Base Imponible : %s', amount_untaxed)
+        _logger.info('Impuestos : %s', amount_tax)
+        _logger.info('Total : %s', amount_total)
+        _logger.info('Margin : %s', internal_margin)
+        _logger.info('Margin in Percent: %s', margin_percent)
+        _logger.info('amount_untaxed: %s', order.amount_untaxed)
 
-        if skip_min_margin_rule:
-            return
-            # _logger.warning('Evitando la regla del MArgen')
-            # if margin_percent <= 0:
-            #     _logger.warning('Margen Negativo, Intentando notificar')
-            #     warning_mess = {
-            #          'title': _('Vendiendo con pérdida'),
-            #          'message': 'Esta venta se está realizando con pérdida'
-            #     }
-            #     return {'warning': warning_mess}
-        elif margin_percent < 0 or margin_percent < max_discount_allowed:
-            raise UserError('El margen del presupuesto esta por debajo de lo esperado.')
+        if margin_percent < 0 or margin_percent < max_discount_allowed:
+            if user.skip_min_margin_rule is False:
+                raise UserError('El margen del presupuesto esta por debajo de lo esperado.')
+            else:
+                self.message_post(body='El usuario cuenta con permisos para vender con pérdida.')
 
         return
+
 
 class CheckMarginOnSalesOrderLine(models.Model):
     _inherit = ['sale.order.line']
@@ -117,12 +112,14 @@ class CheckMarginOnSalesOrderLine(models.Model):
         line = self
         lower_margin = 1.10
         internal_margin = line.purchase_price * lower_margin
+        _logger.warning('Precio del Producto : %s', line.purchase_price)
+        _logger.warning('Precio Minimo de Venta: %s', internal_margin)
+        _logger.warning('Subtotal: %s', line.price_subtotal)
 
         if line.price_subtotal < internal_margin:
+            product_description = line.product_id.default_code + ' - ' + line.product_id.name
             warning_mess = {
                 'title': _('Vendiendo por debajo del costo!'),
-                'message': 'Estas intentando vender por debajo del costo interno. \n\nProducto: ' + line.product_id.name
+                'message': 'Estas intentando vender por debajo del costo interno. \n\nProducto: ' + product_description
             }
             return {'warning': warning_mess}
-
-        return
